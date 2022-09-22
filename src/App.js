4b091a191957866e45ee2b411320b0a891ebf615
Kevin Nielsen <knielsen24@gmail.com>
2022-09-22 09:54:50 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
     return (
-        <div className="container-sm container-fluid">
-            <div className="row">
-                <Navbar />
-            </div>
+        <div className="main-container position-relative container-fluid vh-100 bg-dark-blue">
+            <Navbar />
+            <div className="text-white">this is random content</div>
+
+            <Routes></Routes>
         </div>
     );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-sm bg-light">
+        <nav class="navbar navbar-expand-sm bg-dark-grey mw-75">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">
+                <a class="navbar-brand text-orange" href="/">
                     KN
                 </a>
                 <button
-                    class="navbar-toggler"
+                    class="navbar-toggler bg-white"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNav"
@@ -19,10 +19,10 @@ function Navbar() {
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse " id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav text-white ms-auto">
                         <li class="nav-item">
                             <a
-                                class="nav-link active"
+                                class="nav-link btn text-white"
                                 aria-current="page"
                                 href="#"
                             >
@@ -30,22 +30,22 @@ function Navbar() {
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link btn text-white" href="#">
                                 Projects
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link btn text-white" href="#">
                                 Experience
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link btn text-white" href="#">
                                 Contact
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <button class="btn rounded nav-link" href="#">
+                        <li class="nav-item align-self-center ">
+                            <button class="rounded bg-transparent border-orange text-orange nav-link py-0 px-2" href="#">
                                 Resume
                             </button>
                         </li>
